05770bee5f4089b0b85e28573d987e23be2041ff
Laurent Broudoux <laurent.broudoux@gmail.com>
2023-10-18 17:15:45 +0200

    feat: #24 First iteration on supporting Postman runner Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

---
+++ src/main/java/io/github/microcks/testcontainers/MicrocksContainersEnsemble.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright The Microcks Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.microcks.testcontainers;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.lifecycle.Startable;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.stream.Stream;
+
+/**
+ * An abstraction other a set of containers needed for advanced Microcks services (such as Postman runtime support
+ * or Asynchronous features). Use this when a single {@code MicrocksContainer} is not enough.
+ * @author laurent
+ */
+public class MicrocksContainersEnsemble implements Startable {
+
+   private final Network network;
+
+   private final GenericContainer postman;
+   private final MicrocksContainer microcks;
+
+   /**
+    * Build a new MicrocksContainersEnsemble with its base container image name as string. This image must
+    * be compatible with quay.io/microcks/microcks-uber image.
+    * @param image The name (with tag/version) of Microcks Uber distribution to use.
+    */
+   public MicrocksContainersEnsemble(String image) {
+      this(Network.newNetwork(), image);
+   }
+
+   /**
+    * Build a new MicrocksContainersEnsemble with a pre-existing network and with its base container full image name.
+    * This image must be compatible with quay.io/microcks/microcks-uber image.
+    * @param network The network to attach ensemble containers to.
+    * @param image The name (with tag/version) of Microcks Uber distribution to use.
+    */
+   public MicrocksContainersEnsemble(Network network, String image) {
+      this.network = network;
+      this.microcks = new MicrocksContainer(image)
+            .withNetwork(network)
+            .withNetworkAliases("microcks")
+            .withEnv("POSTMAN_RUNNER_URL", "http://postman:3000")
+            .withEnv("TEST_CALLBACK_URL", "http://microcks:8080");
+
+      this.postman = new GenericContainer<>(DockerImageName.parse("quay.io/microcks/microcks-postman-runtime:latest"))
+            .withNetwork(network)
+            .withNetworkAliases("postman")
+            .waitingFor(Wait.forLogMessage(".*postman-runtime wrapper listening on port.*", 1));;
+   }
+
+   /**
+    * Set host accessibility on ensemble containers.
+    * @param hostAccessible Host accessibility flag
+    * @return The ensemble being built
+    */
+   public MicrocksContainersEnsemble withAccessToHost(boolean hostAccessible) {
+      microcks.withAccessToHost(hostAccessible);
+      postman.withAccessToHost(hostAccessible);
+      return this;
+   }
+
+   /**
+    * Get the Docker network used by this ensemble of Microcks containers.
+    * @return The network ensemble containers are attached to.
+    */
+   public Network getNetwork() {
+      return network;
+   }
+
+   /**
+    * Get the main MicrocksContainer to access the endpoints, artifact management and test methods.
+    * @return The main wrapped MicrocksContainer
+    */
+   public MicrocksContainer getMicrocksContainer() {
+      return microcks;
+   }
+
+   @Override
+   public void start() {
+      // Sequential start to avoid resource contention on CI systems with weaker hardware.
+      microcks.start();
+      postman.start();
+   }
+
+   @Override
+   public void stop() {
+      allContainers().parallel().forEach(GenericContainer::stop);
+   }
+
+   private Stream<GenericContainer<?>> allContainers() {
+      return Stream.of(microcks, postman);
+   }
+}
---
+++ src/test/java/io/github/microcks/testcontainers/MicrocksContainersEnsembleTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright The Microcks Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.microcks.testcontainers;
+
+import io.github.microcks.testcontainers.model.TestRequest;
+import io.github.microcks.testcontainers.model.TestResult;
+import io.github.microcks.testcontainers.model.TestRunnerType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is a test case for MicrocksContainersEnsemble class.
+ * @author laurent
+ */
+public class MicrocksContainersEnsembleTest {
+
+   private static final String IMAGE = "quay.io/microcks/microcks-uber:1.8.0";
+
+   private static final DockerImageName BAD_PASTRY_IMAGE = DockerImageName.parse("quay.io/microcks/contract-testing-demo:02");
+   private static final DockerImageName GOOD_PASTRY_IMAGE = DockerImageName.parse("quay.io/microcks/contract-testing-demo:03");
+
+   @Test
+   public void testPostmanContractTestingFunctionality() throws Exception {
+      try (
+            MicrocksContainersEnsemble ensemble = new MicrocksContainersEnsemble(IMAGE);
+
+            GenericContainer<?> badImpl = new GenericContainer<>(BAD_PASTRY_IMAGE)
+                  .withNetwork(ensemble.getNetwork())
+                  .withNetworkAliases("bad-impl")
+                  .waitingFor(Wait.forLogMessage(".*Example app listening on port 3002.*", 1));
+            GenericContainer<?> goodImpl = new GenericContainer<>(GOOD_PASTRY_IMAGE)
+                  .withNetwork(ensemble.getNetwork())
+                  .withNetworkAliases("good-impl")
+                  .waitingFor(Wait.forLogMessage(".*Example app listening on port 3003.*", 1));
+      ) {
+         ensemble.start();
+         badImpl.start();
+         goodImpl.start();
+         testMicrocksConfigRetrieval(ensemble.getMicrocksContainer().getHttpEndpoint());
+
+         ensemble.getMicrocksContainer().importAsMainArtifact(new File("target/test-classes/apipastries-openapi.yaml"));
+         ensemble.getMicrocksContainer().importAsSecondaryArtifact(new File("target/test-classes/apipastries-postman-collection.json"));
+         testMicrocksContractTestingFunctionality(ensemble.getMicrocksContainer(), badImpl, goodImpl);
+      }
+   }
+
+   private void testMicrocksConfigRetrieval(String endpointUrl) {
+      Response keycloakConfig = RestAssured.given().when()
+            .get(endpointUrl + "/api/keycloak/config")
+            .thenReturn();
+
+      assertEquals(200, keycloakConfig.getStatusCode());
+   }
+
+   private void testMicrocksContractTestingFunctionality(MicrocksContainer microcks, GenericContainer badImpl, GenericContainer goodImpl) throws Exception {
+      // Produce a new test request.
+      TestRequest testRequest = new TestRequest();
+      testRequest.setServiceId("API Pastries:0.0.1");
+      testRequest.setRunnerType(TestRunnerType.POSTMAN.name());
+      testRequest.setTestEndpoint("http://bad-impl:3002");
+      testRequest.setTimeout(2000l);
+
+      // First test should fail with validation failure messages.
+      TestResult testResult = microcks.testEndpoint(testRequest);
+
+      /*
+      System.err.println(microcks.getLogs());
+      ObjectMapper mapper = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
+      System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testResult));
+       */
+
+      assertFalse(testResult.isSuccess());
+      assertEquals("http://bad-impl:3002", testResult.getTestedEndpoint());
+      assertEquals(3, testResult.getTestCaseResults().size());
+      assertTrue(testResult.getTestCaseResults().get(0).getTestStepResults().get(0).getMessage().contains("Valid size in response pastries")
+            || testResult.getTestCaseResults().get(0).getTestStepResults().get(0).getMessage().contains("Valid name in response pastry")
+            || testResult.getTestCaseResults().get(0).getTestStepResults().get(0).getMessage().contains("Valid price in response pastry"));
+
+      // Switch endpoint to the correct implementation.
+      // Other way of doing things via builder and fluent api.
+      TestRequest otherTestRequestDTO = new TestRequest.Builder()
+            .serviceId("API Pastries:0.0.1")
+            .runnerType(TestRunnerType.POSTMAN.name())
+            .testEndpoint("http://good-impl:3003")
+            .timeout(2000L)
+            .build();
+
+      testResult = microcks.testEndpoint(otherTestRequestDTO);
+
+      assertTrue(testResult.isSuccess());
+      assertEquals("http://good-impl:3003", testResult.getTestedEndpoint());
+      assertEquals(3, testResult.getTestCaseResults().size());
+      assertNull(testResult.getTestCaseResults().get(0).getTestStepResults().get(0).getMessage());
+   }
+}
