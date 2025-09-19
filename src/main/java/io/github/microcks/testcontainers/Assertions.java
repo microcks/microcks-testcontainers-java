fd699cad905cdd72203a93acc0f3fac49d688a73
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-09-19 15:14:40 +0200

    feat: #213 Provide TestResult assertion helpers Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

---
+++ src/main/java/io/github/microcks/testcontainers/Assertions.java
@@ -0,0 +1,176 @@
+package io.github.microcks.testcontainers;
+
+import io.github.microcks.testcontainers.model.TestResult;
+
+import org.junit.jupiter.api.AssertionFailureBuilder;
+import org.opentest4j.AssertionFailedError;
+import org.opentest4j.MultipleFailuresError;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Assertion helpers for easily checking TestResult instances.
+ * @author laurent
+ */
+public class Assertions {
+
+   /**
+    * Assert that the given test result is a success. If not, throw an AssertionFailedError with all the
+    * underlying failures from all the test steps in test cases.
+    * @param testResult The test result to check
+    */
+   public static void assertSuccess(TestResult testResult) {
+      List<Throwable> failures = new ArrayList<>();
+      if (!testResult.isSuccess()) {
+         testResult.getTestCaseResults().stream()
+               .filter(testCaseResult -> !testCaseResult.isSuccess())
+               .forEach(testCaseResult -> testCaseResult.getTestStepResults().stream()
+                     .filter(testStepResult -> !testResult.isSuccess())
+                     .forEach(testStepResult -> {
+                        AssertionFailedError failureError = AssertionFailureBuilder.assertionFailure()
+                              .message("Message '" + testStepResult.getRequestName() + "' of operation '"
+                                    + testCaseResult.getOperationName() + "' failed")
+                              .reason(String.join(" && ", testStepResult.getMessage().split("\n")))
+                              .build();
+
+                        failures.add(failureError);
+                     }));
+
+         if (!failures.isEmpty()) {
+            throw new MultipleFailuresError("Test '" + testResult.getId() + "' has failed", failures);
+         }
+      }
+   }
+
+   /**
+    * Assert that the given test result has succeeded for the given operation name. If not, throw an
+    * AssertionFailedError with all the underlying failures from all the test steps in the test case
+    * @param testResult The test result to check
+    * @param operationName The operation name to check (ie. the test case name)
+    */
+   public static void assertSuccess(TestResult testResult, String operationName) {
+      AtomicBoolean foundTestCase = new AtomicBoolean(false);
+      testResult.getTestCaseResults().stream()
+            .filter(testCase -> testCase.getOperationName().equalsIgnoreCase(operationName))
+            .forEach(testCase -> {
+               foundTestCase.set(true);
+               if (!testCase.isSuccess()) {
+                  List<Throwable> failures = new ArrayList<>();
+                  testCase.getTestStepResults().stream()
+                        .filter(testStep -> !testStep.isSuccess())
+                        .forEach(testStep -> {
+                           AssertionFailedError failureError = AssertionFailureBuilder.assertionFailure()
+                                 .message("Message '" + testStep.getRequestName() + "' of operation '"
+                                       + testCase.getOperationName() + "' failed")
+                                 .reason(String.join(" && ", testStep.getMessage().split("\n")))
+                                 .build();
+
+                           failures.add(failureError);
+                        });
+                  if (!failures.isEmpty()) {
+                     throw new MultipleFailuresError("Test '" + testResult.getId() + "' for operation '"
+                           + operationName + "' has failed", failures);
+                  }
+               }
+            });
+      if (!foundTestCase.get()) {
+         throw new AssertionFailedError("Test '" + testResult.getId() + "' has no test case for operation '"
+               + operationName + "'");
+      }
+   }
+
+   /**
+    * Assert that the given test result has succeeded for the given operation name and message name. If not, throw an
+    * AssertionFailedError.
+    * @param testResult The test result to check
+    * @param operationName The operation name to check (ie. the test case name)
+    * @param messageName The message name to check (ie. the test step name)
+    */
+   public static void assertSuccess(TestResult testResult, String operationName, String messageName) {
+      AtomicBoolean foundTestCase = new AtomicBoolean(false);
+      AtomicBoolean foundTestStep = new AtomicBoolean(false);
+      testResult.getTestCaseResults().stream()
+            .filter(testCase -> testCase.getOperationName().equalsIgnoreCase(operationName))
+            .forEach(testCase -> {
+               foundTestCase.set(true);
+               testCase.getTestStepResults().stream()
+                     .filter(testStep -> testStep.getRequestName().equalsIgnoreCase(messageName))
+                     .forEach(testStep -> {
+                        foundTestStep.set(true);
+                        if (!testStep.isSuccess()) {
+                           throw new AssertionFailedError("Test '" + testResult.getId() + "' for operation '"
+                                 + operationName + "' and message '" + messageName + "' has failed: "
+                                 + testStep.getMessage());
+                        }
+                     });
+               if (!foundTestStep.get()) {
+                  throw new AssertionFailedError("Test '" + testResult.getId() + "' has no test step for operation '"
+                        + operationName + "' and message '" + messageName + "'");
+               }
+            });
+      if (!foundTestCase.get()) {
+         throw new AssertionFailedError("Test '" + testResult.getId() + "' has no test case for operation '"
+               + operationName + "'");
+      }
+   }
+
+   /**
+    * Assert that the given test result has failed for the given operation name. If not, throw an
+    * AssertionFailedError.
+    * @param testResult The test result to check
+    * @param operationName The operation name to check (ie. the test case name)
+    */
+   public static void assertFailure(TestResult testResult, String operationName) {
+      AtomicBoolean foundTestCase = new AtomicBoolean(false);
+      testResult.getTestCaseResults().stream()
+            .filter(testCase -> testCase.getOperationName().equalsIgnoreCase(operationName))
+            .forEach(testCase -> {
+               foundTestCase.set(true);
+               if (testCase.isSuccess()) {
+                  throw new AssertionFailedError("Test '" + testResult.getId() + "' for operation '"
+                        + operationName + "' should have failed");
+               }
+            });
+      if (!foundTestCase.get()) {
+         throw new AssertionFailedError("Test '" + testResult.getId() + "' has no test case for operation '"
+               + operationName + "'");
+      }
+   }
+
+   /**
+    * Assert that the given test result has failed for the given operation name and message name. If not, throw an
+    * AssertionFailedError.
+    * @param testResult The test result to check
+    * @param operationName The operation name to check (ie. the test case name)
+    * @param messageName The message name to check (ie. the test step name)
+    */
+   public static void assertFailure(TestResult testResult, String operationName, String messageName) {
+      AtomicBoolean foundTestCase = new AtomicBoolean(false);
+      AtomicBoolean foundTestStep = new AtomicBoolean(false);
+      testResult.getTestCaseResults().stream()
+            .filter(testCase -> testCase.getOperationName().equalsIgnoreCase(operationName))
+            .forEach(testCase -> {
+               foundTestCase.set(true);
+               testCase.getTestStepResults().stream()
+                     .filter(testStep -> testStep.getRequestName().equalsIgnoreCase(messageName))
+                     .forEach(testStep -> {
+                        foundTestStep.set(true);
+                        if (testStep.isSuccess()) {
+                           throw new AssertionFailedError("Test '" + testResult.getId() + "' for operation '"
+                                 + operationName + "' and message '" + messageName + "' should have failed");
+                        }
+                        return;
+                     });
+               if (!foundTestStep.get()) {
+                  throw new AssertionFailedError("Test '" + testResult.getId() + "' has no test step for operation '"
+                        + operationName + "' and message '" + messageName + "'");
+               }
+            });
+      if (!foundTestCase.get()) {
+         throw new AssertionFailedError("Test '" + testResult.getId() + "' has no test case for operation '"
+               + operationName + "'");
+      }
+   }
+}
---
+++ src/test/java/io/github/microcks/testcontainers/AssertionsTest.java
@@ -0,0 +1,151 @@
+package io.github.microcks.testcontainers;
+
+import io.github.microcks.testcontainers.model.TestCaseResult;
+import io.github.microcks.testcontainers.model.TestResult;
+import io.github.microcks.testcontainers.model.TestStepResult;
+
+import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+import org.opentest4j.MultipleFailuresError;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for Assertions.
+ * @author laurent
+ */
+public class AssertionsTest {
+
+   @Test
+   void testFullSuccess() {
+      TestResult result = new TestResult();
+      result.setSuccess(true);
+
+      Assertions.assertSuccess(result);
+   }
+
+   @Test
+   void testPartialSuccess() {
+      TestResult result = new TestResult();
+      result.setId("testPartialSuccess");
+      result.setSuccess(false);
+
+      TestCaseResult case1 = new TestCaseResult();
+      case1.setOperationName("op1");
+      case1.setSuccess(true);
+      TestStepResult step11 = new TestStepResult();
+      step11.setRequestName("req11");
+      step11.setSuccess(true);
+      case1.setTestStepResults(List.of(step11));
+
+      TestCaseResult case2 = new TestCaseResult();
+      case2.setOperationName("op2");
+      case2.setSuccess(false);
+      TestStepResult step21 = new TestStepResult();
+      step21.setRequestName("req21");
+      step21.setSuccess(false);
+      step21.setMessage("Not matching");
+      case2.setTestStepResults(List.of(step21));
+
+      result.setTestCaseResults(List.of(case1, case2));
+
+      boolean failed = false;
+      try {
+         Assertions.assertSuccess(result);
+      } catch (Error error) {
+         failed = true;
+         assertInstanceOf(MultipleFailuresError.class, error);
+         MultipleFailuresError mfe = (MultipleFailuresError)error;
+         assertTrue(mfe.getMessage().startsWith("Test 'testPartialSuccess' has failed (1 failure)"));
+
+         List<Throwable> failures = mfe.getFailures();
+         assertEquals(1, failures.size());
+         assertTrue(failures.get(0).getMessage().equals("Message 'req21' of operation 'op2' failed ==> Not matching"));
+      }
+      if (!failed) {
+         fail("An AssertionFailedError should have been thrown");
+      }
+
+      // Check for operation-specific assertion.
+      Assertions.assertSuccess(result, "op1");
+      Assertions.assertSuccess(result, "op1", "req11");
+
+      failed = false;
+      try {
+         Assertions.assertSuccess(result, "op2");
+      } catch (Error error) {
+         failed = true;
+         assertInstanceOf(MultipleFailuresError.class, error);
+         MultipleFailuresError mfe = (MultipleFailuresError)error;
+         assertTrue(mfe.getMessage().startsWith("Test 'testPartialSuccess' for operation 'op2' has failed (1 failure)"));
+
+         List<Throwable> failures = mfe.getFailures();
+         assertEquals(1, failures.size());
+         assertEquals("Message 'req21' of operation 'op2' failed ==> Not matching", failures.get(0).getMessage());
+      }
+      if (!failed) {
+         fail("An AssertionFailedError should have been thrown");
+      }
+
+      failed = false;
+      try {
+         Assertions.assertSuccess(result, "op1", "req12");
+      } catch (Error error) {
+         failed = true;
+         assertInstanceOf(AssertionFailedError.class, error);
+         assertEquals("Test 'testPartialSuccess' has no test step for operation 'op1' and message 'req12'", error.getMessage());
+      }
+      if (!failed) {
+         fail("An AssertionFailedError should have been thrown");
+      }
+   }
+
+   @Test
+   void testPartialFailure() {
+      TestResult result = new TestResult();
+      result.setId("testPartialFailure");
+      result.setSuccess(false);
+
+      TestCaseResult case1 = new TestCaseResult();
+      case1.setOperationName("op1");
+      case1.setSuccess(true);
+      TestCaseResult case2 = new TestCaseResult();
+      case2.setOperationName("op2");
+      case2.setSuccess(false);
+      TestStepResult step21 = new TestStepResult();
+      step21.setRequestName("req21");
+      step21.setSuccess(false);
+      step21.setMessage("Not matching");
+      case2.setTestStepResults(List.of(step21));
+      result.setTestCaseResults(List.of(case1, case2));
+
+      Assertions.assertFailure(result, "op2");
+      Assertions.assertFailure(result, "op2", "req21");
+
+      boolean failed = false;
+      try {
+         Assertions.assertFailure(result, "op1");
+      } catch (Error error) {
+         failed = true;
+         assertInstanceOf(AssertionFailedError.class, error);
+         assertEquals("Test 'testPartialFailure' for operation 'op1' should have failed", error.getMessage());
+      }
+      if (!failed) {
+         fail("An AssertionFailedError should have been thrown");
+      }
+
+      failed = false;
+      try {
+         Assertions.assertFailure(result, "op2", "req22");
+      } catch (Error error) {
+         failed = true;
+         assertInstanceOf(AssertionFailedError.class, error);
+         assertEquals("Test 'testPartialFailure' has no test step for operation 'op2' and message 'req22'", error.getMessage());
+      }
+      if (!failed) {
+         fail("An AssertionFailedError should have been thrown");
+      }
+   }
+}
